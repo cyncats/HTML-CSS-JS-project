147c71464545b302f33795de7a4d8d828dca9fa6
Cynthia <cyncats@gmail.com>
2022-03-08 09:53:49 +0100

    k

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
+function weatherCalculation()
+{ 
 var TempFah = prompt("What is the temperature in Fahrenheit? ");
 
 var TempCel = (TempFah - 32) * (5/9);
 var TempCelFinal = TempCel.toFixed(1)
 
 console.log("The temperature in Celsius is: " + TempCelFinal)
+output.textContent = weatherCalculation()
 
+}
